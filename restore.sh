85707dea7c380d017153abcdb53b1cb52d139836
80036nd <40246027+80036nd@users.noreply.github.com>
2020-01-02 11:00:58 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-rm -rf FutureRestore
-git clone https://github.com/80036nd/FutureRestore
-chmod +x /FutureRestore/futurerestore
+chmod +x futurerestore
 chmod +x install.sh
 chmod +x main.sh
 chmod +x files/igetnonce
