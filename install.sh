f7bda71ac5741d8d806e480331628020dbb56661
80036nd <40246027+80036nd@users.noreply.github.com>
2020-01-02 10:56:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./futurerestore -t blob.shsh2 --latest-sep --latest-baseband restore.ipsw
+./FutureRestore/futurerestore -t blob.shsh2 --latest-sep --latest-baseband restore.ipsw
 clear
 echo "Your Device Has Been Restored"
