6ecd4928f8a8a7c282eb77d7ab5536801cdea504
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-08-13 08:07:53 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -2,9 +2,9 @@
 chmod +x futurerestore
 chmod +x install.sh
 chmod +x main.sh
-chmod +x files/igetnonce
 chmod +x files/irecovery
 chmod +x files/lsusb
+echo "*** FutureRestore Booter ***"
 ./main.sh
 clear
 echo "********* FutureRestore is Starting **********"
