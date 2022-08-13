2d130a16ce14eec3f1bc2ffe38e6b8ced4a628d7
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-08-13 08:07:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Please drag and drop IPSW file into terminal:"
 read ipsw
-./futurerestore282 -t blob.shsh2 --latest-sep --latest-baseband -d $ipsw
+./futurerestore -t blob.shsh2 --latest-sep --latest-baseband -d $ipsw
 clear
 echo "Device Should be Restored"
 echo "FutureRestore-Script By 80036nd"
