925fa66a88c0a54803edbefff10dbee0eedfc19a
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2022-08-13 09:47:04 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+xattr -d com.apple.quarantine futurerestore
 chmod +x futurerestore
 chmod +x install.sh
 chmod +x main.sh
-chmod +x files/irecovery
-chmod +x files/lsusb
+cd files
+xattr -d com.apple.quarantine irecovery
+xattr -d com.apple.quarantine lsusb
+chmod +x irecovery
+chmod +x lsusb
+cd ..
 echo "*** FutureRestore Booter ***"
 ./main.sh
 clear
